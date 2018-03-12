c595bedb1fb1c86930e3e2749d140c63b09eb7f6
Foglotus <1605104939@qq.com>
2018-03-12 20:20:26 +0800

    1.修改注释 数组->线性顺序表 2.添加在i位置前插入元素方法 insert_sq_by_index

--- Data-Structure/01.Sequence-list.cpp
+++ Data-Structure/01.Sequence-list.cpp
@@ -3,7 +3,7 @@
 #include<math.h>
 using namespace std;
 /*
-顺序表初始化大小
+线性顺序表初始化大小
 顺序表增量
 定义错误代码
 */
@@ -13,7 +13,7 @@ using namespace std;
 #define CORRECT 1
 typedef int ElementType;
 /*
-@name 可变数组组成
+@name 线性顺序表组成
 @ElementType  struct
 @param slist  指针  指向数组。
 @param length int  记录数组已经使用的长度
@@ -26,7 +26,7 @@ struct Sqlist
 	int size;
 };
 /*
-@name 初始化数组
+@name 初始化线性顺序表
 @ElementType method
 @param Sqlist *sqlist 顺序表变量
 @return 1：初始化成功 0：初始化失败
@@ -41,7 +41,7 @@ int	ini_sqlist(Sqlist *sqlist) {
 	return CORRECT;
 }
 /*
-@name 数组多个赋值
+@name 线性顺序表多个赋值
 @ElementType method
 @param Sqlist *sqlist
 @param int count（添加值的个数)
@@ -72,7 +72,7 @@ int sqlist_append_list(Sqlist *sqlist, int count, ...) {
 	return CORRECT;
 }
 /*
-@name 数组单个赋值
+@name 线性顺序表单个赋值
 @ElementType method
 @param Sqlist *sqlist
 @param int ele 添加的值
@@ -94,27 +94,55 @@ int sqlist_append(Sqlist *sqlist, int ele) {
 	return CORRECT;
 }
 /*
-@name 数组打印
+@name 线性顺序表打印
 @ElementType method
 @param Sqlist sqlist
 @param int type 输出模式：0输出已经初始化的，1全部输出，默认0
 @output json
 */
 void print_sqlist(Sqlist sqlist, int type = 0) {
-	cout << "sqlist[" << sqlist.size << "]=\n(\n";
+	cout << "sqlist[" << sqlist.size << "]\n(\n";
 	int number = type == 1 ? sqlist.size : sqlist.length;
 	for (int i = 0; i < number; i++) {
 		cout << "  [" << i << "] = " << sqlist.slist[i] << "\n";
 	}
 	cout << ")\n";
 }
+/*
+@name 在i位置之前插入值
+@ElementType method
+@param Sqlist sqlist
+@param int i 插入的位置
+@param ele 元素
+*/
+int insert_sq_by_index(Sqlist *sqlist,int i,int ele){
+	//判断插入是否合法
+	//1.线性表是否未初始化，插入位置是否错误
+	if (sqlist->slist == NULL || i < 1 || i > sqlist->length) {
+		return ERROR;
+	}
+	if (sqlist->size <= sqlist->length) {
+		sqlist->slist = (ElementType *)realloc(sqlist->slist, sizeof(ElementType)*(sqlist->size + INCREASE_SIZE));
+		sqlist->size += INCREASE_SIZE;
+	}
+	for (int start = sqlist->length-1; start>=i-1; start--) {
+		sqlist->slist[start + 1] = sqlist->slist[start];
+	}
+	sqlist->slist[i - 1] = ele;
+	sqlist->length++;
+	return CORRECT;
+}
 int main() {
 	Sqlist sqlist;
-	ini_sqlist(&sqlist);
-	sqlist_append_list(&sqlist, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8);
-	sqlist_append(&sqlist, 9);
+	ini_sqlist(&sqlist);	
+	sqlist_append_list(&sqlist, 8, 0, 3,4, 5, 6, 7, 8, 9);
 	sqlist_append(&sqlist, 10);
-	sqlist_append(&sqlist, 11);
+	print_sqlist(sqlist);
+	insert_sq_by_index(&sqlist,2,2);
+	print_sqlist(sqlist);
+	insert_sq_by_index(&sqlist, 2, 1);
+	print_sqlist(sqlist);
+	insert_sq_by_index(&sqlist, 11, 11);
 	print_sqlist(sqlist);
 	system("pause");
 	return 0;
